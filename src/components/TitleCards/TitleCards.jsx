c18d8e5df0e80e7c08f15ddb642cac7d0bfedcfc
Steve Splash <stevesplash4@gmail.com>
2025-05-19 05:45:55 -0700

    Homepage + Footer Completed

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './TitleCard.css'
+import card_data from '../../assets/cards/Cards_data';
 
-const TitleCards = () => {
+const TitleCards = ({ title, category }) => {
+    const cardsRef = useRef();
+
+    const handleWheel = (e) => {
+        e.preventDefault();
+        cardsRef.current.scrollLeft += e.deltaY;
+    }
+
+    useEffect(() => {
+        cardsRef.current.addEventListener('wheel', handleWheel);
+    }, []);
+
     return (
-        <div className="titlecards">
-
+        <div className="title-cards">
+            <h2>{title ? title : "Popular on Netflix"}</h2>
+            <div className="card-list" ref={cardsRef}>
+                {
+                    card_data.map((card, index) => {
+                        return <div className="card" key={index}>
+                            <img src={card.image} alt="" />
+                            <p>{card.name}</p>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
