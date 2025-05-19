d272d52f1ec546f9b0b0f18deebc5ea7accc8af6
Steve Splash <stevesplash4@gmail.com>
2025-05-19 01:09:29 -0700

    proj structure setup

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCard.css'
+
+const TitleCards = () => {
+    return (
+        <div className="titlecards">
+
+        </div>
+    )
+}
+
+export default TitleCards
