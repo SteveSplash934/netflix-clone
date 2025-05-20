dfaef5adb12c6e23403fec33ccb4a5a238b89ea3
Steve Splash <stevesplash4@gmail.com>
2025-05-19 17:34:18 -0700

    Done with the Player integration, adding google firebase ft

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [apiData, setApiData] = useState({
+        name: "",
+        key: "",
+        published_at: "",
+        type: ""
+    })
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDMxOTM4NDMxY2U2ZTA3ZWIwOWYyMDQ5MTcxZjM5NCIsIm5iZiI6MTc0NzY2MzAzNi43ODMsInN1YiI6IjY4MmIzOGJjYjYwMGI1YmU0NThjMDdmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TbgqQfRswmKfrzNeP9LeEHh8EGYbdaDle5RD5KrYZFA'
+        }
+    };
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+            .then(res => res.json())
+            .then(res => setApiData(res.results[0]))
+            .catch(err => console.error(err));
+    }, []);
     return (
         <div className='player'>
-            <img src={back_arrow_icon} alt="" />
-            <iframe width='90%' height='90%' src="https://www.youtube.com/embed/YQQD67N5" frameborder="0" title="trailer" allowFullScreen></iframe>
+            <img src={back_arrow_icon} alt="" onClick={() => { navigate(-2) }} />
+            <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} frameBorder="0" title="trailer" allowFullScreen></iframe>
             <div className="player-info">
-                <p>Published Date</p>
-                <p>Name</p>
-                <p>Type</p>
+                <p>{apiData.published_at.slice(0, 10)}</p>
+                <p>{apiData.name}</p>
+                <p>{apiData.type}</p>
             </div>
         </div>
     )
